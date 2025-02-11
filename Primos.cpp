79dbbd1099b4d1e064212a8e9172808fba2211c2
Nilber-Mayta <lpze.nilber.mayta.cu@unifranz.edu.bo>
2025-02-11 17:50:13 -0400

    Diagnostico

--- Primos.cpp
+++ Primos.cpp
@@ -1,28 +1,23 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 bool esPrimo(int n) {
-    if (n < 2) return false;
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0) return false;
+
+    if (n == 1 )
+        return false;
+
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0)
+            return false;
     }
-    return true;
+      return true;
 }
 
 int main() {
-    int limite;
-
-    cout << "Ingrese el límite para la serie de números primos: ";
-    cin >> limite;
-
-    cout << "Números primos hasta " << limite << ":\n";
-    for (int i = 2; i <= limite; i++) {
-        if (esPrimo(i)) {
+    cin>> int n ;
+    for (int i = 1; i <= n; i++) {
+        if (esPrimo(i))
             cout << i << " ";
-        }
     }
-    cout << endl;
-
     return 0;
 }
